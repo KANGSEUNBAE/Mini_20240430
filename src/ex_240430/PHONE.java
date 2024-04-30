59fad7593227cb1021a97b8d45959fe596ed119c
KANGSEUNBAE <KANGSEUNGBAE00@gmail.com>
2024-04-30 15:48:39 +0900

    first commit

---
+++ src/ex_240430/PHONE.java
@@ -0,0 +1,14 @@
+package ex_240430;
+
+public interface PHONE {
+	public final static String PHONENAME = "갤럭시";
+	public abstract void call(String callMember);
+	public abstract void message(String[] messageMember);
+	public abstract void camera(String[] camerMode);
+	public abstract void internet(String[] Browser);
+
+}
+
+
+
+
