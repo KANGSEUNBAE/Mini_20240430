0c3633ed09a3eb1267acd92282529b9c72d29aca
KANGSEUNGBAE <164992129+KANGSEUNBAE@users.noreply.github.com>
2024-04-30 16:06:18 +0900

    Update and rename PHONE.java to Phone.java

--- src/ex_240430/Phone.java
+++ src/ex_240430/Phone.java
@@ -1,14 +1,12 @@
 package ex_240430;
 
 public interface PHONE {
+	
 	public final static String PHONENAME = "갤럭시";
+	// 주요 기능 : 1) 전화 2) 메시지 3) 카메라 4) 인터넷
 	public abstract void call(String callMember);
 	public abstract void message(String[] messageMember);
 	public abstract void camera(String[] camerMode);
 	public abstract void internet(String[] Browser);
 
 }
-
-
-
-
