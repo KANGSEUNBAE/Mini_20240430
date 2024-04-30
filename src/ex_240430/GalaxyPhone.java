7ed151e66189f2e5422774fd41c069ac0f380558
heeji-lee <dpgbanjdal@gmail.com>
2024-04-30 16:03:51 +0900

    ㄷㅁㄱㄷㅈㄹㄷㄴ

---
+++ src/ex_240430/GalaxyPhone.java
@@ -0,0 +1,10 @@
+package ex_240430;
+
+public interface GalaxyPhone extends PHONE {
+	
+	public final static String PhonePrice = "기본 1,000,000원";
+	
+	// 기능 : 1) 용량 선택하기
+	public abstract void selectMemorySize(int selectMemorySizeNumber);
+
+}
